e1fb20258af9b5d8d60f3dd7a03e509d985d81e9
charlot2106 <charlotdemers21@gmail.com>
2021-04-14 17:27:15 -0400

    Ajout des fonctions du capteur MAX30102 ainsi que les fichiers headers et C de la tâche de détection d'accélération à l'aide du bmi160. Inclut aussi des fichiers de librairies I2C et BMI160.

---
+++ MAX30102_functions.h
@@ -0,0 +1,41 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+
+#define MAX_ADDR        (0x57)
+#define MAX_FIFO_ADDR   (0x07)
+
+#include "project.h"
+#include "bmi160.h"
+#include "bmi160_defs.h"
+#include "motion_task.h"
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "i2cm_support.h"
+#include "task.h"
+#include <stdio.h>
+
+//Fonctions i2c
+uint8 read_register_MAX(uint8 baseAddress);
+
+bool write_register_MAX(uint8_t baseAddress, uint8_t data);
+
+void readMultipleBytes_MAX(uint8_t baseAddress, uint8_t *buffer, uint8_t length);
+
+
+//Fonctions MAX
+
+bool MAX_Reset();
+
+void read_MAX30102();
+
+/* [] END OF FILE */
