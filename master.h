f910c03955373478f8dfb20d8d17935789709ca8
jadehenrichon <jadehenrichon@hotmail.com>
2021-04-15 15:13:06 -0400

    Supression des fichiers inutiles et ajout de master.h et Traitement_Signal.c

--- master.h
+++ master.h
@@ -12,7 +12,20 @@
 #include "project.h"
 #include <arm_math.h>
 #include <core_cm4.h>
+#include <stdlib.h>
 
-uint32 periode(uint32 signal[], uint32 max[], uint32 indice[]);
+void filtre_2_75s(float32 signal[], float32 signal_filtre[]);
+
+void filtre_PB_AC(float32 signal[], float32 signal_filtre[]);
+
+float32 AC(float32 signal[]);
+
+float32 DC_sum(float32 signal[]);
+
+void SPO2_task(void* pvParameters);
+
+void heart_rate_task(void* pvParameters);
+
+
 
 /* [] END OF FILE */
